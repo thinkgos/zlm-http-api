5dc897fe6cc63c9afa6a93243c46c35e3df99713
thinkgo <thinkgo@aliyun.com>
2025-06-30 12:01:32 +0800

    fix: fix comment

--- zlm_http/streams_close.go
+++ zlm_http/streams_close.go
@@ -9,10 +9,10 @@ import (
 
 type CloseStreamsRequest struct {
 	Secret string `json:"secret,omitempty"` // O, api操作密钥(配置文件配置), 未填则忽略, 可设置全局参数或token来统一传.
-	Schema string `json:"schema,omitempty"` // O, 筛选协议, 例如rtsp或 rtmp
-	Vhost  string `json:"vhost,omitempty"`  // O, 筛选虚拟主机, 例如__defaultVhost__
-	App    string `json:"app,omitempty"`    // O, 筛选应用名, 例如 live
-	Stream string `json:"stream,omitempty"` // O, 筛选流id, 例如 test
+	Schema string `json:"schema,omitempty"` // O, 筛选协议, 例如: rtsp或rtmp
+	Vhost  string `json:"vhost,omitempty"`  // O, 筛选虚拟主机, 例如: __defaultVhost__
+	App    string `json:"app,omitempty"`    // O, 筛选应用名, 例如: live
+	Stream string `json:"stream,omitempty"` // O, 筛选流id, 例如: test
 	Force  *int   `json:"force,omitempty"`  // O, 是否强制关闭(有人在观看将强制关闭), 0: 不强制关闭, 1: 强制关闭
 }
 type CloseStreamsReply struct {
